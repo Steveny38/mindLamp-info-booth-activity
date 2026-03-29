929080f62633e658bb6e9d36606541581f7e25da
Yu Hang <yuhangmak123@gmail.com>
2026-03-29 18:36:29 -0400

    Testing out vite for custom activity

--- situational-awareness/compress_activity.sh
+++ situational-awareness/compress_activity.sh
@@ -4,14 +4,6 @@
 
 name=$(basename "$(pwd)")
 
-mkdir -p "../dist/in" "../dist/out"
-
-npx --yes --package inline-source-cli inline-source \
-  --attribute "" \
-  --compress false \
-  --root ./dist \
-  ./dist/index.html > "../dist/in/$name.html"
-
-openssl base64 -A \
-  -in "../dist/in/$name.html" \
-  -out "../dist/out/$name.html.b64"
+cat dist/index.html \
+  | tee "../dist/in/$name.html" \
+  | openssl base64 > "../dist/out/$name.html.b64"
