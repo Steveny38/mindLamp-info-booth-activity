422e21362910f8b7746fd465c8506dc8531c8bde
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2026-04-17 06:24:21 -0400

    Delete situational-awareness directory

--- situational-awareness/compress_activity.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# npx --yes --package inline-source-cli inline-source --attribute "" --compress false --root ./build ./build/index.html > dist.html
-# openssl base64 -in dist.html -out dist.html.b64
-
-name=$(basename "$(pwd)")
-
-cat dist/index.html \
-  | tee "../dist/in/$name.html" \
-  | openssl base64 > "../dist/out/$name.html.b64"
